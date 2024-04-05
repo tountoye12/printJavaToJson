8f1542802140d99636fbc9f85fa31351e62c32a7
diallo <mamadouadama609@gmail.com>
2024-04-05 01:36:17 -0500

    creating comparator by name and by age for sorting emplyee && add prettyPrinting for Json

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -17,28 +17,32 @@ public class Main {
         List<Employee> employeeList1 = List.of(
                 new Employee(1, "Adama Diallo", 24, LocalDate.now()),
                 new Employee(2, "John Doe", 27, LocalDate.now()),
-                new Employee(2, "John Doe", 22, LocalDate.now()),
-                new Employee(3, "John Ly", 17, LocalDate.now())
+                new Employee(3, "John Doe", 22, LocalDate.now()),
+                new Employee(4, "John Ly", 17, LocalDate.now()),
+                new Employee(5, "Ebrima Doe", 22, LocalDate.now()),
+                new Employee(6, "Gabou Ly", 17, LocalDate.now())
         );
 
-        printEmployee(employeeList1);
+        List<Employee> employeeListSorted = sortEmployeeList(employeeList1);
+        printEmployeeJson(employeeListSorted);
     }
 
-    private static void printEmployee(List<Employee> employeeList) {
-
-        employeeList.stream()
-                .sorted(
-                        Comparator.comparing(Employee::getName)
-                                .thenComparing(Employee::getAge)
-                );
-
+    private static void printEmployeeJson(List<Employee> employeeList) {
         Gson gson = new GsonBuilder().registerTypeAdapter(
                 LocalDate.class, new LocalDateTypeAdapter()
-        ).create();
-
+        ).setPrettyPrinting().create();
         JsonElement jsonElement = gson.toJsonTree(employeeList);
         String json = gson.toJson(jsonElement);
         System.out.println(json);
+    }
+
+    private static List<Employee> sortEmployeeList(List<Employee> employeeList) {
+        Comparator<Employee> comparatorByName = Comparator.comparing(Employee::getName);
+        Comparator<Employee> comparatorByAge = Comparator.comparing(Employee::getAge);
+        return employeeList.stream()
+                .sorted(
+                        comparatorByName.thenComparing(comparatorByAge)
+                ).toList();
     }
 
 
