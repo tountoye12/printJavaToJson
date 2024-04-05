322ba8b04f14bd118a8e45dfd14fd9d06766250e
A-Daffeh <adama.daffeh07@gmail.com>
2024-04-05 00:00:13 -0500

    Updated the json to print in pretty format

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,26 +9,28 @@ import org.example.helper.LocalDateTypeAdapter;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
+
+import static java.time.LocalDate.now;
 
 public class Main {
     public static void main(String[] args) {
 
 
         List<Employee> employeeList1 = new ArrayList<>();
-        employeeList1.add(new Employee(1, "Adama Diallo", 24, java.time.LocalDate.now()));
-        employeeList1.add(new Employee(2, "John Doe", 27, java.time.LocalDate.now()));
-        employeeList1.add(new Employee(3, "Ebrima Doe", 22, java.time.LocalDate.now()));
-        employeeList1.add(new Employee(4, "Gabou Ly", 17, java.time.LocalDate.now()));
+        employeeList1.add(new Employee(1, "Adama Diallo", 24, now()));
+        employeeList1.add(new Employee(2, "John Doe", 27, now()));
+        employeeList1.add(new Employee(3, "Ebrima Doe", 22, now()));
+        employeeList1.add(new Employee(4, "Gabou Ly", 17, now()));
 
         Collections.sort(employeeList1);
         printEmployee(employeeList1);
     }
 
     private static void printEmployee(List<Employee> employeeList) {
-
-        Gson gson = new GsonBuilder().registerTypeAdapter(
+        Gson gson = new GsonBuilder()
+                .setPrettyPrinting()
+                .registerTypeAdapter(
                 LocalDate.class, new LocalDateTypeAdapter()
         ).create();
 
